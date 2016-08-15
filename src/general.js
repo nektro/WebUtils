ffaedc7f2bd2c75feec159be18c919c27660694e
Sean D <nektromc@gmail.com>
2016-08-15 09:47:19 -0400

    Update general.js

--- src/general.js
+++ src/general.js
@@ -1,3 +1,8 @@
+/**
+ * Adds a <script> to the page
+ * @param {String} the URL of the script file
+ * @param {Function} [optional] a callback function for when the file has finsished loading
+ */
 function addScript(url,on) {
     var script = document.createElement('script');
     if (on !== undefined) script.addEventListener('load', function(e) { on(e); });
