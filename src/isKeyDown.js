788e85d9b4d7df860826c15f775d36ca3f82968e
Sean D <nektromc@gmail.com>
2016-08-15 09:48:48 -0400

    Update isKeyDown.js

--- src/isKeyDown.js
+++ src/isKeyDown.js
@@ -11,6 +11,11 @@
     window.addEventListener('keyup', (e) => {
         keyStates[`k${e.keyCode}`] = false;
     });
+    /**
+     * Adds two numbers
+     * @param {Number} a key ID
+     * @return {Boolean} whether or not the specified key is currently being pressed
+     */
     window.isKeyDown = function(k) {
         var v = keyStates[`k${k}`];
 
