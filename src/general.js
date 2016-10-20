3f93a63f12d92be1f3f746d69976b026bfa25f36
Sean D <nektromc@gmail.com>
2016-10-19 21:56:08 -0400

    Update general.js

--- src/general.js
+++ src/general.js
@@ -1,8 +1,3 @@
-/**
- * Adds a <script> to the page
- * @param {String} url: the URL of the script file
- * @param {Function} [optional] on: a callback function for when the file has finsished loading
- */
 function addScript(url,on) {
     var script = document.createElement('script');
     if (on !== undefined) script.addEventListener('load', function(e) { on(e); });
@@ -10,10 +5,6 @@ function addScript(url,on) {
     script.setAttribute('src',url);
     document.head.appendChild(script);
 }
-/**
- * @param {Object} o: an object
- * @returns {Array} an array of prototypes defined by the Inheritance tree of {o}
- */
 function getInheritance(o) {
     var it = [];
     var p = o;
