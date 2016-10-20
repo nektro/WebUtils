42414e74c8148901b1560032699b2228cb56d5ec
Sean D <nektromc@gmail.com>
2016-10-19 21:08:04 -0400

    Update general.js
    add getInheritance(o)

--- src/general.js
+++ src/general.js
@@ -1,7 +1,7 @@
 /**
  * Adds a <script> to the page
- * @param {String} the URL of the script file
- * @param {Function} [optional] a callback function for when the file has finsished loading
+ * @param {String} url: the URL of the script file
+ * @param {Function} [optional] on: a callback function for when the file has finsished loading
  */
 function addScript(url,on) {
     var script = document.createElement('script');
@@ -9,4 +9,18 @@ function addScript(url,on) {
     script.async = false;
     script.setAttribute('src',url);
     document.head.appendChild(script);
+}
+/**
+ * @param {Object} o: an object
+ * @returns {Array} an array of prototypes defined by the Inheritance tree of {o}
+ */
+function getInheritance(o) {
+    var it = [];
+    var p = o;
+    while (p !== null) {
+        if (p !== o)
+            it.push(p);
+        p = p.__proto__;
+    }
+    return it;
 }
