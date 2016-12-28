b1bb1d1c606eb78ee54da6a6e52b620fdbe30ed7
Sean D <nektromc@gmail.com>
2016-12-28 01:59:35 -0500

    Update keyboard.js

--- src/keyboard.js
+++ src/keyboard.js
@@ -11,10 +11,20 @@
         codes: {
         },
         isKeyDown: function(k) {
+            if (keyStates.has(k))
+                if (keyStates.get(k) === 1)
+                    return true;
+            return false;
         },
         areKeysDown: function(ka) {
+            for (var y in ka)
+                if (keyStates.has(y))
+                    if (keyStates.get(y) === 0)
+                        return false;
+            return true;
         },
         getKeyMap: function() {
+            return keyStates;
         }
     };
 })();
