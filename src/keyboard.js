fc7c26fa1d7d4ed4b958273d30478e6871d45b2a
Sean D <nektromc@gmail.com>
2016-12-28 01:58:37 -0500

    Update keyboard.js

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,5 +1,11 @@
 (function() {
     var KeyStates = new Map();
+    window.addEventListener('keydown', (e) => {
+        keyStates.set(e.keyCode, 1);
+    });
+    window.addEventListener('keyup', (e) => {
+        keyStates.set(e.keyCode, 0);
+    });
     
     window.KeyHelp = {
         codes: {
