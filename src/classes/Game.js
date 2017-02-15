7e998f9a0e611841c9c599dab09d836d39ae43a0
Sean D <nektromc@gmail.com>
2017-02-15 16:56:01 -0500

    Create Game.js

---
+++ src/classes/Game.js
@@ -0,0 +1,46 @@
+/*
+ * @author Nektro (Sean Denny)
+ * Copyright (c) 2016-2017
+ */
+'use strict';
+class GameObject {
+    constructor() {
+        this.gstates = new Array();
+        this.activeState = -1;
+        
+        window.addEventListener('keydown', (e) => {
+            if (this.activeState >= 0) {
+                this.gstates[this.activeState].onKeyPressed(e.keyCode);
+            }
+        });
+    }
+    addState(gs) {
+        if (gs instanceof GameState) {
+            this.gstates.push(gs);
+        }
+    }
+    start() {
+        this.gotoState(0);
+    }
+    gotoState(s) {
+        this.activeState = s;
+        this.gstates[this.activeState].init();
+        this.loop();
+    }
+    next() {
+        this.activeState += 1;
+    }
+    loop() {
+        var st = this.gstates[this.activeState];
+        st.update();
+        st.draw();
+        requestAnimationFrame(this.loop.bind(this));
+    }
+}
+class GameState {
+    constructor() { }
+    init() { }
+    update() { }
+    draw() { }
+    onKeyPressed() { }
+}
