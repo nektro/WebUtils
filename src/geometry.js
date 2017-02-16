b8e8cc3be778eb868ef644b9d201fe87ab09896a
Sean D <nektromc@gmail.com>
2017-02-15 20:16:04 -0500

    Update geometry.js
    Add class Point

--- src/geometry.js
+++ src/geometry.js
@@ -1,5 +1,14 @@
 /*
  * @author Nektro (Sean Denny)
- * Copyright (c) 2016-2017
+ * Copyright (c) 2017
  */
 'use strict';
+
+class Point {
+    constructor(a, b, c, d) {
+        this.x = a | 0;
+        this.y = b | 0;
+        this.z = c | 0;
+        this.w = d | 0;
+    }
+}
