f6d3f87c065e2439f65b3cfdeff595573f4ab1ba
Sean D <nektromc@gmail.com>
2017-02-15 21:25:52 -0500

    Update geometry.js
    fix intercept algo and add distanceTo to Point

--- src/geometry.js
+++ src/geometry.js
@@ -11,6 +11,9 @@ class Point {
         this.x = a | 0;
         this.y = b | 0;
     }
+    distanceTo(pt) {
+        return Math.sqrt(Math.pow(pt.x - this.x, 2) + Math.pow(pt.y - this.y, 2));
+    }
 }
 
 class Circle {
@@ -19,9 +22,12 @@ class Circle {
         this.y = b | 0;
         this.r = c | 0;
     }
+    get center() {
+        return new Point(this.x, this.y);
+    }
     intersects(ob) {
         if (ob instanceof Point) // circle x point via https://math.stackexchange.com/a/198769
-            return Math.sqrt(Math.pow(ob.x + this.x) + Math.pow(ob.y + this.y)) <= this.r;
+            return ob.distanceTo(this.center) <= this.r;
         return false;
     }
 }
