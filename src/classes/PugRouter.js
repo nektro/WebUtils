6b7dd0db2e274312a7c89264710841d5d0547ce2
Nektro <nektromc@gmail.com>
2017-06-30 01:41:29 -0400

    add resolveFileName to PugRouter
    This is an overrideable function that can used to tell PR where the pug files are

--- src/classes/PugRouter.js
+++ src/classes/PugRouter.js
@@ -6,17 +6,21 @@
 class PugRouter {
     constructor(r, pa) {
         this.root = r;
-        this.pages = pa;
+        this.pages = pa || new Array();
         this.pages.push('/');
         this.cache = new Map();
         window.addEventListener('hashchange', (e) => {
             this.gotoPage(e.newURL.substring(e.newURL.indexOf('#') + 1));
         });
     }
+    resolveFileName(rt) {
+        if (this.pages.indexOf(rt) > -1) return `${rt}.pug`;
+    }
     getPugFileName(pn) {
-        if (pn === '/') return Promise.resolve('/index');
-        if (this.pages.indexOf(pn) > -1) return Promise.resolve(pn);
-        return Promise.resolve('/blank');
+        if (pn === '/') return Promise.resolve('/index.pug');
+        let rsn = this.resolveFileName(pn);
+        if (rsn !== undefined) return Promise.resolve(rsn);
+        return Promise.resolve('/blank.pug');
     }
     getPageContent(p) {
         return this.getPugFileName(p).then((pl) => {
@@ -24,7 +28,7 @@ class PugRouter {
                 return Promise.resolve(this.cache.get(pl));
             }
             else {
-                return fetch(`${this.root}/pages${pl}.pug`).then((x) => { return x.text() }).then((x) => {
+                return fetch(`${this.root}/pages${pl}`).then((x) => { return x.text() }).then((x) => {
                     let file = pug.compile(x)();
                     if (!(this.cache.has(pl))) this.cache.set(pl, file);
                     return Promise.resolve(file);
