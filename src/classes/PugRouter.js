cff838e4c3e339f51a7dd51fd4419f955b4ccc54
Nektro <nektromc@gmail.com>
2017-06-30 02:37:09 -0400

    Allow PugRouter to send and recieve options parameter
    When overriding resolveFileName() and setPageContent()

--- src/classes/PugRouter.js
+++ src/classes/PugRouter.js
@@ -14,31 +14,31 @@ class PugRouter {
         });
     }
     resolveFileName(rt) {
-        if (this.pages.indexOf(rt) > -1) return `${rt}.pug`;
+        if (this.pages.indexOf(rt) > -1) return [`${rt}.pug`];
     }
     getPugFileName(pn) {
-        if (pn === '/') return Promise.resolve('/index.pug');
+        if (pn === '/') return Promise.resolve(['/index.pug']);
         let rsn = this.resolveFileName(pn);
         if (rsn !== undefined) return Promise.resolve(rsn);
-        return Promise.resolve('/blank.pug');
+        return Promise.resolve(['/blank.pug']);
     }
     getPageContent(p) {
         return this.getPugFileName(p).then((pl) => {
-            if (this.cache.has(pl)) {
-                return Promise.resolve(this.cache.get(pl));
+            if (this.cache.has(pl[0])) {
+                return Promise.resolve([this.cache.get(pl[0]),pl[1]]);
             }
             else {
-                return fetch(`${this.root}/pages${pl}`).then((x) => { return x.text() }).then((x) => {
+                return fetch(`${this.root}/pages${pl[0]}`).then((x) => { return x.text() }).then((x) => {
                     let file = pug.compile(x)();
-                    if (!(this.cache.has(pl))) this.cache.set(pl, file);
-                    return Promise.resolve(file);
+                    if (!(this.cache.has(pl[0]))) this.cache.set(pl[0], file);
+                    return Promise.resolve([file,pl[1]]);
                 });
             }
         });
     }
     gotoPage(pn) {
         this.getPageContent(pn).then((x) => {
-            this.setPageContent(x);
+            this.setPageContent(...x);
         });
     }
     setPageContent(con) {
