d44527cffe9b40de478c5bed904ead03e2d960fe
Nektro <nektromc@gmail.com>
2017-10-08 17:26:35 -0400

    update isKeyDown(k) to use Set

--- src/isKeyDown.js
+++ src/isKeyDown.js
@@ -3,33 +3,29 @@
 * Copyright (c) 2016
 */
 (function() {
-    var keyStates = new Map();
+    var keyStates = new Set();
 
     window.addEventListener('keydown', (e) => {
-        keyStates.set(e.keyCode, 1);
+        keyStates.add(e.keyCode);
     });
     window.addEventListener('keyup', (e) => {
-        keyStates.set(e.keyCode, 0);
+        keyStates.delete(e.keyCode);
     });
     /**
      * @param {Number} a key ID
      * @return {Boolean} whether or not the specified key is currently being pressed
      */
     window.isKeyDown = function(k) {
-        if (keyStates.has(k))
-            if (keyStates.get(k) === 1)
-                return true;
-        return false;
+        return keyStates.has(k);
     };
     /**
      * @param {Array<Number>} an array of key IDs
      * @return {Boolean} whether or not all of the keys in the array are being pressed
      */
     window.areKeysDown = function(ka) {
-        for (var y in ka)
-            if (keyStates.has(y))
-                if (keyStates.get(y) === 0)
-                    return false;
+        for (var y of ka)
+            if (keyStates.has(y) === false)
+				return false;
         return true;
     };
 })();
